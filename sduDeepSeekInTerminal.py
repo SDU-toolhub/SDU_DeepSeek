167aa816c312bff34c9aa21321ba46b2dbc7e6e5
zeroHYH <HWANG_YH@outlook.com>
2025-03-04 00:07:26 +0800

    Add feature: login in terminal

--- sduDeepSeekInTerminal.py
+++ sduDeepSeekInTerminal.py
@@ -1,50 +1,41 @@
-import time, requests, os, toml, json
-from selenium import webdriver
+import requests, os, toml, json
 from rich.console import Console
 from rich.markdown import Markdown
-from datetime import datetime
+from datetime import datetime, timezone
+from getpass import getpass
+from sdu_aiassist_login import login
 
 if os.path.exists("SDUTAIconfigs.toml"):
     with open("SDUTAIconfigs.toml", "r") as f:
         configs = toml.load(f)
         f.close()
-    cookies = configs["cookies"]
+    if configs["expires"] > datetime.now(timezone.utc):
+        cookies = configs["cookies"]
+    else:
+        exit(print("Cookie expired, please delete SDUTAIconfigs.toml"))
 else:
-    driver = webdriver.Edge()
-    driver.get("https://aiassist.sdu.edu.cn/")
-    while not driver.current_url.startswith(
-        "https://aiassist.sdu.edu.cn/page/site/newPc"
-    ):
-        time.sleep(1)
-    cookies = {cookie["name"]: cookie["value"] for cookie in driver.get_cookies()}
-    driver.quit()
-    configs = {"cookies": cookies, "online": False, "reason": True}
+    sduid = input("SDU ID: ")
+    password = getpass("Password: ")
+    fingerprint = input("Device fingerprint(none for random uuid): ")
+    configs = login(sduid, password, fingerprint)
+    configs["online"] = False
+    configs["reason"] = True
 
-commands = {
-    "exit": {"message": "Goodbye!", "exec": ""},
-    "clear": {"message": "History cleared.", "exec": """history = []"""},
-    "online": {
-        "message": "Internet search enabled.",
-        "exec": """configs["online"] = True""",
-    },
-    "offline": {
-        "message": "Internet search disabled.",
-        "exec": """configs["online"] = False""",
-    },
-    "r1": {
-        "message": "Model switched to deepseek-r1.",
-        "exec": """configs["reason"] = True""",
-    },
-    "v3": {
-        "message": "Model switched to deepseek-v3.",
-        "exec": """configs["reason"] = False""",
-    },
-    "help": {
-        "message": "exit, clear, online, offline, r1, v3, help",
-        "exec": "",
-    },
-}
-print("SDUTAI Chatbot\nType :help for a list of commands.")
+commands = {"exit": {"message": "Goodbye!", "exec": ""},
+            "clear": {"message": "History cleared.",
+                      "exec": """history = []"""},
+            "online": {"message": "Internet search enabled.",
+                       "exec": """configs["online"] = True"""},
+            "offline": {"message": "Internet search disabled.",
+                        "exec": """configs["online"] = False"""},
+            "r1": {"message": "Model switched to deepseek-r1.",
+                   "exec": """configs["reason"] = True"""},
+            "v3": {"message": "Model switched to deepseek-v3.",
+                   "exec": """configs["reason"] = False"""},
+            "help": {
+                "message": "usage\n\t:command\ncommands\n\texit, clear, online, offline, r1, v3, help",
+                "exec": ""}}
+print("SDU DeepSeek in Terminal\nType :help for a list of commands.")
 history = []
 
 while True:
@@ -62,22 +53,16 @@ while True:
             print("Invalid command. Type :help for a list of commands.")
         continue
     form_data = {
-        "compose_id": 73,
-        "auth_tag": "本科生",
+        "compose_id": 73, "auth_tag": "本科生", "content": content,
         "deep_search": 1 if configs["reason"] else 2,
-        "internet_search": 1 if configs["online"] else 2,
-        "content": content,
-    }
+        "internet_search": 1 if configs["online"] else 2}
     for i, chat_session in enumerate(history):
         form_data[f"history[{i}][role]"] = chat_session["role"]
         form_data[f"history[{i}][content]"] = chat_session["content"]
     time_start = datetime.now()
     response = requests.post(
         "https://aiassist.sdu.edu.cn/site/ai/compose_chat",
-        data=form_data,
-        cookies=cookies,
-        stream=True,
-    )
+        data=form_data, cookies=cookies, stream=True)
     answer = ""
     for line in response.iter_lines():
         if line:
@@ -93,10 +78,9 @@ while True:
     print(f"\n\nGenerated in {time_delta.total_seconds():.2f} seconds")
     console = Console(width=80)
     if configs["reason"]:
-        answer = answer[answer.find("</think>") + 10 :]
+        answer = answer[answer.find("</think>") + 10:]
     if "\n" in answer:
         console.print(Markdown(answer))
-
 
 with open("SDUTAIconfigs.toml", "w") as f:
     toml.dump(configs, f)
