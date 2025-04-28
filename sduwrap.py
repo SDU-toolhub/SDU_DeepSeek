5bcee9d486d20dd29dbb8432cbd1372f816db87d
futz12 <1391525377@qq.com>
2025-04-29 01:42:50 +0800

    修复由于api逻辑变换导致的bug（要求 user对话 和assistant对话 数量相等）

--- sduwrap.py
+++ sduwrap.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+from PIL.ImageChops import offset
+
 cookies = {}
 
 url = "https://aiassist.sdu.edu.cn/site/ai/compose_chat"
@@ -22,19 +24,34 @@ class ChatConfig:
 
 def history_to_form_data(history):
     form_data = {}
+    offset = 0
+
     for i, chat_session in enumerate(history):
-        form_data[f"history[{i}][role]"] = chat_session.role
-        form_data[f"history[{i}][content]"] = chat_session.content
+        if chat_session.role == "system":
+            form_data[f"history[{i + offset}][role]"] = "user"
+            form_data[f"history[{i + offset}][content]"] = chat_session.content
+
+            offset += 1
+            form_data[f"history[{i + offset}][role]"] = "assistant"
+            form_data[f"history[{i + offset}][content]"] = "我知道了"
+
+            continue
+
+        form_data[f"history[{i+offset}][role]"] = chat_session.role
+        form_data[f"history[{i+offset}][content]"] = chat_session.content
+
     return form_data
 
 
 def make_chat_request(content, history, config):
-    form_data = history_to_form_data(history)
+    form_data = {}
     form_data["content"] = content
+    form_data.update(history_to_form_data(history))
     form_data["compose_id"] = config.compose_id
     form_data["auth_tag"] = config.auth_tag
     form_data["deep_search"] = config.deep_search
     form_data["internet_search"] = config.internet_search
+
     return form_data
 
 
@@ -43,8 +60,10 @@ def chat(content, history, config):
     # response = requests.post(url, data=form_data, cookies=cookies,verify=False)
 
     # 流式输出
-    response = requests.post(url, data=form_data, cookies=cookies, stream=True, verify=False) # 防止开了代理没法用
+    response = requests.post(url, data=form_data, cookies=cookies, stream=True, verify=False)  # 防止开了代理没法用
+
     for line in response.iter_lines():
+
         if line:
             # line.decode('utf-8') - data: {"e":0,"m":"操作成功","d":{"type":"0","answer":"<think>好的","url":"","message_id":"","id":"","recommend_data":[],"source":[],"ext":[]}}
             text = line.decode('utf-8')
